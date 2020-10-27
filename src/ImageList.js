4af8684220e3383e535eb6c7386d07ae350e6d85
Shane Upchurch <shaneupchurch@gmail.com>
2020-10-27 18:17:22 -0500

    fixing eslint errors

--- src/ImageList.js
+++ src/ImageList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import ImageItem from './ImageItem.js'
-import images from './data.js'
+
 
 export default class ImageList extends Component {
     state = {
